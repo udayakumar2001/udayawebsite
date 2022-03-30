5022f6759df2adfcaa23a8a9da15b5da82c34f30
udayakumar2001 <102378183+udayakumar2001@users.noreply.github.com>
2022-03-31 00:12:33 +0530

    Update contact.php

--- web/contact.php
+++ web/contact.php
@@ -1,11 +1,13 @@
 <?php
 // database connection code
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-$host ="ec2-54-160-109-68.compute-1.amazonaws.com"
-$port =" port = 5432"
-$dbname ="dbname = d94vb9dvd4ste3"
-$credentials =" user =ydbnlatsdtsmte  password =7f3f7f6e8e94eaed6c1dc6d5cd538f7af3427fe24f762ecc7362a5c5d678735b"
-$con = pg_connect( "$host $port $dbname $credentials");
+
+$host        = "host=ec2-18-215-96-22.compute-1.amazonaws.com";
+$port        = "port=5432";
+$dbname      = "dbname = d1uttm14vli79m";
+$credentials = "user = ahrahipezdqduv password=68f14fa49232785fb28dfe160fae7d1e7973d4246514af497f1860fd0b84e8a8";
+
+$con = pg_connect( "$host $port $dbname $credentials"  );
 
 // get the post records
 $txtName = $_POST['txtName'];
@@ -14,16 +16,17 @@ $txtPhone = $_POST['txtPhone'];
 $txtMessage = $_POST['txtMessage'];
 
 // database insert SQL code
-$sql = "INSERT INTO `tbl_contact` (fldName, fldEmail, fldPhone, fldMessage) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage');";
+$sql = "INSERT INTO tbl_contact(fldName, fldEmail, fldPhone, fldMessage) VALUES ('$txtName', '$txtEmail', '$txtPhone', '$txtMessage');";
+//echo $sql;
 
 // insert in database 
 $rs = pg_query($con, $sql);
 
 if($rs)
 {
-	echo "Contact Records Inserted";
+  echo "Contact Records Inserted";
 }else{
-echo "Contact not Inserted";
+  echo "Contact Not Inserted";
 }
 
 ?>
